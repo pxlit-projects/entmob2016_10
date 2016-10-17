8a9f8f2d07e9b0cb5087cb9bd1f43857b152fa65
PieterSwennen <pieter.swennen@student.pxl.be>
2016-10-17 16:16:31 +0200

    Controller

--- JavaSpringApi/src/main/java/be/pxl/Main.java
+++ JavaSpringApi/src/main/java/be/pxl/Main.java
@@ -1,8 +1,11 @@
 package be.pxl;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by Pieter on 17/10/2016.
  */
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
---
+++ JavaSpringApi/src/main/java/be/pxl/controllers/StrongPlateController.java
@@ -0,0 +1,13 @@
+package be.pxl.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Pieter on 17/10/2016.
+ */
+@Controller
+@RequestMapping("/GetData")
+public class StrongPlateController {
+    
+}
