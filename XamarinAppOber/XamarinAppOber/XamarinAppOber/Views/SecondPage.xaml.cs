110aec819ac74b4ded5f94c8d1fd741f81804159
Simonnnn <Simon.geelen@hotmail.com>
2016-10-24 13:10:33 +0200

    Using monkey robotics

--- XamarinAppOber/XamarinAppOber/XamarinAppOber/Views/SecondPage.xaml.cs
+++ XamarinAppOber/XamarinAppOber/XamarinAppOber/Views/SecondPage.xaml.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 
-namespace XamarinAppOber
+namespace XamarinAppOber.Views
 {
-    public partial class MainPage : ContentPage
+    public partial class SecondPage : ContentPage
     {
-        public MainPage()
+        public SecondPage()
         {
             InitializeComponent();
         }
