892868c7e3830ad140781289cb7b32792d50bebc
Simonnnn <Simon.geelen@hotmail.com>
2016-11-04 16:43:15 +0100

    zef

--- XamarinApp/App1/App1/Pages/MainPage.xaml.cs
+++ XamarinApp/App1/App1/Pages/MainPage.xaml.cs
@@ -14,7 +14,7 @@ namespace App1
         public MainPage(IAdapter adapter,IBluetoothLE ble)
         {
             InitializeComponent();
-            BindingContext = new MyViewModel(adapter,ble);
+            BindingContext = new DeviceViewModel(adapter,ble);
 
         }
     }
