f0f0e61f3915dfd4aec3576d8be9f5982568a6bb
jeroenroumen <jeroen.roumen@gmail.com>
2016-11-20 12:40:00 +0100

    overbodige projecten verwijderd

---
+++ XamarinApp/App1.Tests/ViewModel/ServiceListViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App1.Tests.ViewModel
+{
+    [TestClass]
+    public class ServiceListViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
