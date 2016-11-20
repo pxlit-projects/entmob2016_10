ce6150b79ada6c85f92277b7b44368df89deeb53
jeroenroumen <jeroen.roumen@gmail.com>
2016-11-20 12:48:43 +0100

    IValueConverter unit test

---
+++ XamarinApp/App1.Tests/Converters/RoundConverterTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App1.Converter;
+
+namespace App1.Tests.Converters
+{
+    [TestClass]
+    public class RoundConverterTest
+    {
+        [TestMethod]
+        public void TestRoundConverter()
+        {
+            //arrange
+            RoundConverter roundConverter = new RoundConverter();
+            var actual = roundConverter.Convert(2.5478, typeof(double) , null, null);
+
+            //test
+            Assert.AreEqual(2.55, actual);
+        }
+    }
+}
