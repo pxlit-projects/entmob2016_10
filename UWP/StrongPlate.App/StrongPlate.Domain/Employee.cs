d568a3616571fa4a6896eaebf49e175f9456bca2
evivanlooy <Evi.VanLooy@student.pxl.be>
2016-11-21 15:29:06 +0100

    ImageGender

--- UWP/StrongPlate.App/StrongPlate.Domain/Employee.cs
+++ UWP/StrongPlate.App/StrongPlate.Domain/Employee.cs
@@ -15,7 +15,7 @@ namespace StrongPlate.Domain
         public string FirstName { get; set; }
         public string FullName { get; set; }
         public int Age { get; set; }
-        public bool Male { get; set; }
+        public bool Gender { get; set; }
         public string Password { get; set; }
 
         public double averageSpeed;
