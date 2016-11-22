c4f5b8425254b820218f67dcec1d96cf3202196a
Simonnnn <Simon.geelen@hotmail.com>
2016-11-22 14:38:59 +0100

    Mission: Change name #2 Success

--- XamarinApp/App1/App1.Droid/MainActivity.cs
+++ XamarinApp/App1/App1.Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Plugin.Permissions;
 
 namespace App1.Droid
 {
-    [Activity(Label = "App1", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "StrongPlate", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
